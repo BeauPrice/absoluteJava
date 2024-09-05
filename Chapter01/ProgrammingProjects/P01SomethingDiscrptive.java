564c1d6061ec5aeb4b4bc9757e34000ff236e422
BeauPrice <142942752+BeauPrice@users.noreply.github.com>
2024-09-05 16:26:12 -0500

    example of git commit

--- Chapter01/ProgrammingProjects/P01SomethingDiscrptive.java
+++ Chapter01/ProgrammingProjects/P01SomethingDiscrptive.java
@@ -7,11 +7,12 @@ public class P01SomethingDiscrptive {
     public static void main(String[] args) {
         int a = 5;
         int b = 3;
-        int c = a+b;
+        int c = b+a;
 
         System.out.println("hello world!");
         System.out.println();
 
               
+
     }
 }
