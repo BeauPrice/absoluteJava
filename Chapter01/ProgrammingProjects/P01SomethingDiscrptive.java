a7f787db55866a66b03076da54c6100bc41979be
BeauPrice <142942752+BeauPrice@users.noreply.github.com>
2024-09-05 16:23:34 -0500

    Create P01SomethingDiscrptive.java

---
+++ Chapter01/ProgrammingProjects/P01SomethingDiscrptive.java
@@ -0,0 +1,17 @@
+package Chapter01.ProgrammingProjects;
+
+/**
+ * P01SomethingDiscrptive
+ */
+public class P01SomethingDiscrptive {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int c = a+b;
+
+        System.out.println("hello world!");
+        System.out.println();
+
+              
+    }
+}
